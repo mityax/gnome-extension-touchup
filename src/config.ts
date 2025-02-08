20f74521ef5839f34c78ff4f7f32504b3529f868
mityax <mityax@gitlab.com>
2025-02-08 22:19:10 +0100

    feature(donations): Finalized donations page

--- src/config.ts
+++ src/config.ts
@@ -13,3 +13,39 @@ export const assetPath = Object.freeze({
     root: 'resource:///org/gnome/shell/extensions/gnometouch',
     icon: (name: string) => `resource:///org/gnome/shell/extensions/gnometouch/icons/scalable/actions/${name}.svg`,
 });
+
+/**
+ * All platforms users can use to donate
+ */
+export const donationPlatforms = Object.freeze([
+    {
+        name: 'Ko-fi',
+        url: 'https://ko-fi.com/mityax',
+        description: 'Most payment methods, one-time or recurring donations, no sign up required.',
+        recommended: true,
+    },
+    {
+        name: 'Patreon',
+        url: 'https://www.patreon.com/mityax',
+        description: 'Many payment methods, best for a recurring donation.',
+    },
+    {
+        name: 'Buy Me A Coffee',
+        url: 'https://buymeacoffee.com/mityax',
+        description: 'Donate by card, no sign up required.',
+    },
+]);
+
+export const feedbackPlatforms = Object.freeze([
+    {
+        title: 'Create an Issue on GitHub',
+        url: 'https://github.com/mityax/gnome-touch/issues/new',
+        buttonLabel: 'Create Issue',
+    },
+    // TODO: provide this to make it show up in the settings
+    //{
+    //    title: 'Leave a Review on Gnome Extensions',
+    //    url: 'todo',
+    //    buttonLabel: 'Leave Review',
+    //},
+]);
--- src/features/preferences/pages/donationsPage.ts
+++ src/features/preferences/pages/donationsPage.ts
@@ -3,9 +3,9 @@ import GObject from "gi://GObject";
 import Gtk from "gi://Gtk";
 import {buildPreferencesGroup} from "$src/features/preferences/uiUtils.ts";
 import {randomChoice} from "$src/utils/utils.ts";
-import NaturalWrapMode = Gtk.NaturalWrapMode;
-import Orientation = Gtk.Orientation;
-import Align = Gtk.Align;
+import Gio from "gi://Gio";
+import Pango from "@girs/pango-1.0";
+import {donationPlatforms, feedbackPlatforms} from "$src/config.ts";
 
 
 export class DonationsPage extends Adw.PreferencesPage {
@@ -20,22 +20,34 @@ export class DonationsPage extends Adw.PreferencesPage {
         });
 
         this.add(buildPreferencesGroup({
-            title: 'Support GnomeTouch',
+            title: '',
             children: [
                 this.buildInfoBox(),
             ]
+        }));
+        this.add(buildPreferencesGroup({
+            title: 'Support by Donation',
+            description: 'Choose a platform below to make a donation',
+            children: [
+                this.buildDonateOptions(),
+            ],
+        }));
+        this.add(buildPreferencesGroup({
+            title: 'Leave Feedback or Ideas',
+            description: 'Do you like Gnome Touch? Leave your feedback, rating or ideas!',
+            children: this.buildFeedbackCards(),
         }));
     }
 
     private buildInfoBox() {
         const box = new Gtk.Box({
-            cssClasses: ['callout', 'callout--green'],
-            orientation: Orientation.VERTICAL,
+            cssClasses: ['callout', 'card' /*, 'callout--green'*/],
+            orientation: Gtk.Orientation.VERTICAL,
         });
 
         box.append(new Gtk.Label({
             cssClasses: ['title-2'],
-            halign: Align.START,
+            halign: Gtk.Align.START,
             label: randomChoice([
                 'Our Mission', 'Why should I donate?', 'What we believe in'
             ]),
@@ -43,16 +55,84 @@ export class DonationsPage extends Adw.PreferencesPage {
 
         box.append(new Gtk.Label({
             wrap: true,
-            naturalWrapMode: NaturalWrapMode.WORD,
+            naturalWrapMode: Gtk.NaturalWrapMode.WORD,
             hexpand: true,
             label:
-                'Mobile platforms are dominated by corporate-controlled systems, and GNOME itself is tough to use on ' +
-                'touch devices in everyday life. This extension helps improve GNOME’s usability on tablets, making it ' +
-                'a more viable option for touch-based devices. By donating, you’re supporting a project that values ' +
-                'freedom and user choice over profit-driven ecosystems.\n\nEvery contribution helps keep open ' +
-                'platforms competitive and accessible — thank you for making a difference! ❤️'
+                'Mobile platforms are dominated by big tech oligarchs, and Gnome itself is tough to use on touch ' +
+                'devices in everyday life. Gnome Touch helps improve Gnome’s usability on touchscreen devices and ' +
+                'makes it a viable, free alternative: tablet users are no longer forced to sacrifice user experience' +
+                'for freedom, control and privacy.\n\nBy donating, you’re supporting a project that helps users regain ' +
+                'control over their own devices and gives them another option than profit-driven ecosystems. ' +
+                'Every contribution – be it code, monetary, suggestions or feedback – helps keep open software ' +
+                'such as Gnome Touch competitive and accessible.\n\nThank you for making a difference! ❤️'
         }));
 
         return box;
+    }
+
+    private buildDonateOptions() {
+        const box = new Gtk.FlowBox({
+            orientation: Gtk.Orientation.HORIZONTAL,
+            column_spacing: 10,
+            row_spacing: 10,
+            selectionMode: Gtk.SelectionMode.NONE,
+            homogeneous: true,
+        });
+
+        for (let p of donationPlatforms) {
+            const card = new Gtk.Box({
+                cssClasses: ['card', 'padding-normal', 'donations-page__donation-option'],
+                orientation: Gtk.Orientation.VERTICAL,
+            });
+
+            card.append(new Gtk.Label({ label: p.name, cssClasses: ['donations-page__donation-option__title'] }));
+
+            if (p.recommended) {
+                card.add_css_class('donations-page__donation-option--recommended');
+                card.append(new Gtk.Label({
+                    label: '★ recommended',
+                    halign: Gtk.Align.CENTER,
+                    cssClasses: ['donations-page__donation-option__badge'],
+                }));
+            }
+
+            card.append(new Gtk.Label({
+                label: p.description,
+                cssClasses: ['body'],
+                wrapMode: Pango.WrapMode.WORD_CHAR,
+                wrap: true,
+                justify: Gtk.Justification.CENTER,
+                maxWidthChars: 10,
+                vexpand: true,
+            }));
+
+            const btn = new Gtk.Button({ label: `Use ${p.name}`, halign: Gtk.Align.CENTER });
+            btn.connect('clicked', () => Gio.AppInfo.launch_default_for_uri(p.url, null));
+            card.append(btn);
+
+            box.append(card);
+        }
+
+        return box;
+    }
+
+    private buildFeedbackCards() {
+        const res = [];
+
+        for (let r of feedbackPlatforms) {
+            const card = new Gtk.Box({
+                cssClasses: ['card', 'padding-normal'],
+                marginBottom: 10,
+                orientation: Gtk.Orientation.HORIZONTAL,
+            });
+            card.append(new Gtk.Label({ label: r.title }));
+            card.append(new Gtk.Separator({ hexpand: true, cssClasses: ['spacer'] }));
+            const btn = new Gtk.Button({ label: r.buttonLabel });
+            btn.connect('clicked', () => Gio.AppInfo.launch_default_for_uri(r.url, null));
+            card.append(btn);
+            res.push(card);
+        }
+
+        return res;
     }
 }
