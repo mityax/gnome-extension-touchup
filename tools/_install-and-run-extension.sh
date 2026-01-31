3b61bac757e3086c75efc3fa4d91cc61f3570589
mityax <mityax@gitlab.com>
2026-01-31 12:13:11 +0100

    refactor(tools): Clean up _install-and-run-extension.sh script

--- tools/_install-and-run-extension.sh
+++ tools/_install-and-run-extension.sh
@@ -4,23 +4,24 @@ set -euo pipefail
 #
 # This script is meant to be run from within "run.sh" only. Its task is to
 #  1. Remove the currently installed extension, if any
-#  2. Invoke a script passed via the environment variable "PROCESS_SCRIPT",
+#  2. Install the latest extension zip file from the build folder
+#  3. Invoke a script passed via the environment variable "PROCESS_SCRIPT",
 #     that is assumed start GNOME Shell
-#  3. Install the latest extension zip file from the build folder
 #
 # Notably, this script is the entrypoint for the dbus session that is spawned
 # for the Shell; Thus, any code that needs to work within the same DBus session
 # must be put in here.
 #
 
+# Configuration:
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+cd "$SCRIPT_DIR"
+
+source config.sh
+
+# Helpers:
 die() { log "$@" >&2; exit 1; }
 log() { echo -e "\033[1m\033[34m[touchup-wrapper]\033[0m $*"; }
-
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-cd "$SCRIPT_DIR" || die "Cannot cd into $SCRIPT_DIR"
-
-[[ -f config.sh ]] || die "Missing config.sh in $SCRIPT_DIR"
-source config.sh
 
 # Find newest .zip in $distDir
 shopt -s nullglob
@@ -40,6 +41,6 @@ gnome-extensions install -f "$zipFile" || die "Failed to install extension"
 log "Enabling extension (via id $EXTENSION_ID)..."
 gnome-extensions enable "$EXTENSION_ID" || die "Failed to enable extension"
 
-# Run the script in the background
+# Run the script that will start GNOME Shell:
 bash -c "$PROCESS_SCRIPT"
 
